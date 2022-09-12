09b00335c999aadb0e12705517d6bcebfb31b8c3
viggostarcke <vist@itu.dk>
2022-09-12 21:03:09 +0200

    C# Generics solution

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -1,5 +1,24 @@
+using System.Text;
+using Xunit;
+
 namespace Assignment1.Tests;
 
 public class IteratorsTests
 {
+    [Fact]
+    public void Flatten_should_return_1_2_3_4_from_1_2_array_and_3_4_array()
+    {
+        //arrange
+        int[] testArray1 = { 1, 2 };
+        int[] testArray2 = { 3, 4 };
+        List<int[]> testList = new List<int[]>();
+        testList.Add(testArray1);
+        testList.Add(testArray2);
+
+        //act
+        var result = Iterators.Flatten(testList);
+
+        //assert
+        Assert.Equal(new[] {1,2,3,4}, result);
+    }
 }
--- Assignment1/Iterators.cs
+++ Assignment1/Iterators.cs
@@ -1,8 +1,19 @@
+using System.IO.Compression;
+
 namespace Assignment1;
 
 public static class Iterators
 {
-    public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items) => throw new NotImplementedException();
-
+    public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items)
+    {
+        foreach (var v in items)
+        {
+            foreach (var _v in v)
+            {
+                yield return _v;
+            }
+        }
+    }
+    
     public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate) => throw new NotImplementedException();
 }
