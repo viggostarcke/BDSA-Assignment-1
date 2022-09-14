d8c1c863f323dd54c36a9a3cdeceeeb0610897dd
Viggo Starcke <vist@itu.dk>
2022-09-14 16:25:47 +0200

    regex resolution solution implemented

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -7,5 +7,44 @@ using FluentAssertions;
 public class RegExprTests
 {
     
+    [Fact]
+    public void SplitLine_returns_1_string_from_1_arrays()
+    {
+        //arrange
+        string[] testArray1 = { "to!", "be# or not", "#€@to be" };
+
+        //act
+        var result = RegExpr.SplitLine(testArray1);
+
+        //assert
+        result.Should().BeEquivalentTo(new [] { "to", "be", "or", "not", "to", "be" });
+    }
+
+    [Fact]
+    public void Resolution_returns_tuples_from_stream() 
+    {
+        //arrange
+        string[] testArray = { "1920x1080", "1024x768, 800x600" };
+
+        //act
+        var result = RegExpr.Resolution(testArray);
+
+        //assert
+        result.Should().BeEquivalentTo(new [] { (1920, 1080), (1024, 768), (800, 600) });
+    }
+
+    [Fact]
+    public void InnerText_returns_inner_text_of_HTML_tags()
+    {
+        //arrange
+        string html = "<h2>Flower</h2>";
+        string tag = "h2";
+
+        //act
+        var result = RegExpr.InnerText(html, tag);
+
+        //assert
+        result.Should().BeEquivalentTo("Flower");
+    }
 
 }
--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -1,13 +1,44 @@
 namespace Assignment1;
 
+using System.Text.RegularExpressions;
+
 public static class RegExpr
 {
     public static IEnumerable<string> SplitLine(IEnumerable<string> lines) 
     {
-        throw new NotImplementedException();
+        var pattern = @"([a-zA-Z0-9]+)";
+
+        foreach (var v in lines) {
+            var matches = Regex.Matches(v, pattern);
+            foreach (var m in matches) 
+            {
+                if (m != null) 
+                {
+                    yield return m.ToString();
+                }
+            }
+        }
     }
 
-    public static IEnumerable<(int width, int height)> Resolution(string resolutions) => throw new NotImplementedException();
+    public static IEnumerable<(int width, int height)> Resolution(IEnumerable<string> resolutions)
+    {
+        var pattern = @"(?<width>[0-9]+)x(?<height>[0-9]+)";
+
+        foreach (var v in resolutions) 
+        {
+            var matches = Regex.Matches(v, pattern);
+
+            foreach (Match _v in matches)
+            {
+                int width;
+                int height;
+                int.TryParse(_v.Groups["width"].Value, out width);
+                int.TryParse(_v.Groups["height"].Value, out height);
+
+                yield return (width, height);
+            }
+        }
+    }
 
     public static IEnumerable<string> InnerText(string html, string tag) => throw new NotImplementedException();
 }
